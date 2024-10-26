7c37e9c2b274df53f1d0aaec96e58247232bbbe1
Tobias Nylund <tobiasnylund@live.se>
2024-10-26 16:34:14 +0200

    Commented out HttpsRedirection

--- Program.cs
+++ Program.cs
@@ -53,10 +53,7 @@ if (app.Environment.IsDevelopment())
   app.UseSwaggerUI();
 }
 
-if (!app.Environment.IsDevelopment())
-{
-  app.UseHttpsRedirection();
-}
+ //app.UseHttpsRedirection();
 
 app.UseAuthorization();
 app.UseCors("AllowFrontend");
